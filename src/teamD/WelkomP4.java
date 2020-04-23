0ce226f2ae30c55e2c588baddb08655d9ea26077
ROOJ <ROOJ@192.168.178.178>
2020-04-23 13:09:07 +0200

    Eerste code

---
+++ src/teamD/WelkomP4.java
@@ -0,0 +1,15 @@
+package teamD;
+
+public class WelkomP4 {
+
+	public WelkomP4() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hallo P4 !! ");
+	}
+
+}
